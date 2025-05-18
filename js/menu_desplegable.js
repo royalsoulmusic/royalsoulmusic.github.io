675c33bba13bbd39f1a7dddda770918e7cc563ae
royalsoulmusic <userbot003m@gmail.com>
2025-05-18 11:45:32 -0400

    Add files via upload

---
+++ js/menu_desplegable.js
@@ -0,0 +1,9 @@
+const menu_deplegable = document.getElementById('menu_deplegable');
+
+function abrir_menu_desplegable(){
+    menu_deplegable.style.display = 'flex';
+}
+
+function cerrar_menu_desplegable(){
+    menu_deplegable.style.display = 'none';
+}
